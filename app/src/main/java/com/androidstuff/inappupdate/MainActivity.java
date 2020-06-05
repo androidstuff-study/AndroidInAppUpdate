b299913fb14d0f59151d4ea9ff43f61d2de74be7
Sonakshi Priya <sonakshipriya2017@gmail.com>
2020-06-06 00:07:01 +0530

    Implemented flexible in-app update features.

---
+++ app/src/main/java/com/androidstuff/inappupdate/MainActivity.java
@@ -0,0 +1,164 @@
+package com.androidstuff.inappupdate;
+
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.InstallState;
+import com.google.android.play.core.install.InstallStateUpdatedListener;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.InstallStatus;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.OnSuccessListener;
+import com.google.android.play.core.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int FLEXIBLE_APP_UPDATE_REQ_CODE = 123;
+    private static final int IMMEDIATE_APP_UPDATE_REQ_CODE = 124;
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+    private AppUpdateManager appUpdateManager;
+    private InstallStateUpdatedListener installStateUpdatedListener;
+
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        appUpdateManager = AppUpdateManagerFactory.create(getApplicationContext());
+
+        installStateUpdatedListener = new
+                InstallStateUpdatedListener() {
+                    @Override
+                    public void onStateUpdate(InstallState state) {
+                        if (state.installStatus() == InstallStatus.DOWNLOADED){
+                            popupSnackbarForCompleteUpdate();
+                        } else if (state.installStatus() == InstallStatus.INSTALLED){
+                            removeInstallStateUpdateListener();
+                        } else {
+                            Log.i(TAG, "InstallStateUpdatedListener: state: " + state.installStatus());
+                        }
+                    }
+                };
+        appUpdateManager.registerListener(installStateUpdatedListener);
+        checkUpdate();
+
+        Name = findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnlogin);
+        Info.setText("No of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void removeInstallStateUpdateListener() {
+        if (appUpdateManager != null) {
+            appUpdateManager.unregisterListener(installStateUpdatedListener);
+        }
+    }
+
+    private void  checkUpdate () {
+        // Returns an intent object that you use to check for update
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+        // checks that the platform will allow the specified type of update:
+        appUpdateInfoTask.addOnSuccessListener(new OnSuccessListener<AppUpdateInfo>() {
+            @Override
+            public void onSuccess(AppUpdateInfo appUpdateInfo) {
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                        && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+                    startUpdateFlow(appUpdateInfo, AppUpdateType.FLEXIBLE, FLEXIBLE_APP_UPDATE_REQ_CODE);
+                } else if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                    popupSnackbarForCompleteUpdate();
+                }
+            }
+        });
+    }
+
+    private void startUpdateFlow (AppUpdateInfo appUpdateInfo, int updateType, int reqCode) {
+        try {
+            appUpdateManager.startUpdateFlowForResult(appUpdateInfo, updateType, this, reqCode);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == FLEXIBLE_APP_UPDATE_REQ_CODE){
+            if (resultCode == RESULT_CANCELED) {
+                // DO NOTHING
+                Log.d("", "Update canceled by user! Result Code: " + resultCode);
+            } else if (resultCode == RESULT_OK){
+                Log.d("", "Update success! Result Code: " + resultCode);
+            } else {
+                Log.d ("", "Update Failed! Result Code: " + resultCode);
+                checkUpdate();
+            }
+        }
+    }
+
+    private  void validate(String userName, String userPassword){
+        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter--;
+            Info.setText("No of attempts remaining:"+ String.valueOf(counter));
+            if(counter ==  0){
+                Login.setEnabled(false);
+            }
+        }
+    }
+
+    private void popupSnackbarForCompleteUpdate() {
+
+
+
+        Snackbar snackbar =
+                Snackbar.make(
+                        (CoordinatorLayout) findViewById(R.id.snackbar_layout),
+                        "New app is ready!",
+                        Snackbar.LENGTH_INDEFINITE);
+
+        snackbar.setAction("Install", view -> {
+            if (appUpdateManager != null){
+                appUpdateManager.completeUpdate();
+            }
+        });
+
+
+        snackbar.setActionTextColor(getResources().getColor(R.color.installColor));
+        snackbar.show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        removeInstallStateUpdateListener();
+    }
+}
